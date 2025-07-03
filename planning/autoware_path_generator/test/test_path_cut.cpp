3586dca10a2d7dcf4c1840b6caa1b699a8793c52
mitukou1109 <mitukou1109@gmail.com>
2025-07-03 15:38:24 +0900

    add tests for get_lanelet_sequence_covering_path()
    Signed-off-by: mitukou1109 <mitukou1109@gmail.com>

--- planning/autoware_path_generator/test/test_path_cut.cpp
+++ planning/autoware_path_generator/test/test_path_cut.cpp
@@ -127,6 +127,80 @@ TEST_F(UtilsTest, getPathBound)
   }
 }
 
+TEST_F(UtilsTest, getLaneletSequenceCoveringPath)
+{
+  set_map("autoware_test_utils", "2km_test.osm");
+  constexpr auto epsilon = 1e-1;
+
+  {  // lanelet sequence is empty
+    const auto result = utils::get_lanelet_sequence_covering_path({}, {PathPointWithLaneId{}}, {});
+
+    ASSERT_FALSE(result.has_value());
+  }
+
+  {  // path points are empty
+    const auto result =
+      utils::get_lanelet_sequence_covering_path(get_lanelets_from_ids({4417}), {}, {});
+
+    ASSERT_FALSE(result.has_value());
+  }
+
+  {  // normal case
+    PathPointWithLaneId start, end;
+    start.point.pose.position.x = -999.0;
+    start.point.pose.position.y = 1.75;
+    start.lane_ids = {4417};
+    end.point.pose.position.x = -976.0;
+    end.point.pose.position.y = 1.75;
+    end.lane_ids = {4417};
+    const auto result = utils::get_lanelet_sequence_covering_path(
+      get_lanelets_from_ids({4417}), {start, end}, planner_data_.routing_graph_ptr);
+
+    ASSERT_TRUE(result.has_value());
+    ASSERT_EQ(result->lanelet_sequence.size(), 1);
+    ASSERT_EQ(result->lanelet_sequence[0].id(), 4417);
+    ASSERT_NEAR(result->s_start, 1.0, epsilon);
+    ASSERT_NEAR(result->s_end, 24.0, epsilon);
+  }
+
+  {  // normal case with missing lanelet sequence
+    PathPointWithLaneId start, end;
+    start.point.pose.position.x = -974.0;
+    start.point.pose.position.y = 1.75;
+    start.lane_ids = {4429};
+    end.point.pose.position.x = -926.0;
+    end.point.pose.position.y = 1.75;
+    end.lane_ids = {4434};
+    const auto result = utils::get_lanelet_sequence_covering_path(
+      get_lanelets_from_ids({4429}), {start, end}, planner_data_.routing_graph_ptr);
+
+    ASSERT_TRUE(result.has_value());
+    ASSERT_GE(result->lanelet_sequence.size(), 2);
+    ASSERT_EQ(result->lanelet_sequence[0].id(), 4429);
+    ASSERT_EQ(result->lanelet_sequence[1].id(), 4434);
+    ASSERT_NEAR(result->s_start, 1.0, epsilon);
+    ASSERT_NEAR(result->s_end, 49.0, epsilon);
+  }
+
+  {  // normal case with redundant lanelet sequence
+    PathPointWithLaneId start, end;
+    start.point.pose.position.x = -974.5;
+    start.point.pose.position.y = 1.75;
+    start.lane_ids = {4429};
+    end.point.pose.position.x = -950.5;
+    end.point.pose.position.y = 1.75;
+    end.lane_ids = {4429};
+    const auto result = utils::get_lanelet_sequence_covering_path(
+      get_lanelets_from_ids({4417, 4429, 4434}), {start, end}, planner_data_.routing_graph_ptr);
+
+    ASSERT_TRUE(result.has_value());
+    ASSERT_EQ(result->lanelet_sequence.size(), 1);
+    ASSERT_EQ(result->lanelet_sequence[0].id(), 4429);
+    ASSERT_NEAR(result->s_start, 0.5, epsilon);
+    ASSERT_NEAR(result->s_end, 24.5, epsilon);
+  }
+}
+
 TEST_F(UtilsTest, cropLineString)
 {
   constexpr auto epsilon = 1e-1;
